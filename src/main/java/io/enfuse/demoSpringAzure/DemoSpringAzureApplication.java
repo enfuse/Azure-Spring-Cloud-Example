6f450948ea86d3cdd2dca3fe5782a47c45ac5fd9
tl-enfuse <tl@enfuse.io>
2022-06-29 13:39:31 -0400

    initial commit

---
+++ src/main/java/io/enfuse/demoSpringAzure/DemoSpringAzureApplication.java
@@ -0,0 +1,13 @@
+package io.enfuse.demoSpringAzure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/enfuse/demoSpringAzure/DemoSpringAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.enfuse.demoSpringAzure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
